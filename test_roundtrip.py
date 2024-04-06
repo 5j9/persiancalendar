372ce768e0ca2f1e969670705ddfc5338dc2bf93
5j9 <5j9@users.noreply.github.com>
2024-04-06 20:38:40 +0330

    tests: rename date var to ordinal

--- test_roundtrip.py
+++ test_roundtrip.py
@@ -1,5 +1,10 @@
-import persiancalendar
-import persiancalendar_fast
+from persiancalendar import persian_from_ordinal, ordinal_from_persian
+from persiancalendar_fast import (
+    persian_fast_from_ordinal,
+    ordinal_from_persian_fast,
+    SUPPORTED_FIRST_YEAR,
+    SUPPORTED_LAST_YEAR,
+)
 
 ROUNDTRIP_START_YEAR = 1304
 ROUNDTRIP_END_YEAR = 1500
@@ -7,39 +12,33 @@ ROUNDTRIP_END_YEAR = 1500
 
 def test_roundtrip_astronomical():
     """Test that the astronomical algorithm roundtrips correctly."""
-    start = persiancalendar.ordinal_from_persian(ROUNDTRIP_START_YEAR, 1, 1)
-    end = persiancalendar.ordinal_from_persian(ROUNDTRIP_END_YEAR, 1, 1)
+    start = ordinal_from_persian(ROUNDTRIP_START_YEAR, 1, 1)
+    end = ordinal_from_persian(ROUNDTRIP_END_YEAR, 1, 1)
 
-    for date in range(start, end):
-        p_date = persiancalendar.persian_from_ordinal(date)
-        converted_back = persiancalendar.ordinal_from_persian(*p_date)
-        assert (date == converted_back)
+    for ordinal in range(start, end):
+        p_date = persian_from_ordinal(ordinal)
+        converted_back = ordinal_from_persian(*p_date)
+        assert ordinal == converted_back
 
 
 def test_roundtrip_fast():
     """Test that the fast algorithm roundtrips correctly."""
-    start = persiancalendar_fast.ordinal_from_persian_fast(
-        ROUNDTRIP_START_YEAR, 1, 1)
-    end = persiancalendar_fast.ordinal_from_persian_fast(
-        ROUNDTRIP_END_YEAR, 1, 1)
+    start = ordinal_from_persian_fast(ROUNDTRIP_START_YEAR, 1, 1)
+    end = ordinal_from_persian_fast(ROUNDTRIP_END_YEAR, 1, 1)
 
-    for date in range(start, end):
-        p_date = persiancalendar_fast.persian_fast_from_ordinal(date)
-        converted_back = persiancalendar_fast.ordinal_from_persian_fast(*p_date)
-        assert (date == converted_back)
+    for ordinal in range(start, end):
+        p_date = persian_fast_from_ordinal(ordinal)
+        converted_back = ordinal_from_persian_fast(*p_date)
+        assert ordinal == converted_back
 
 
 def test_fast():
     """Test that the results of the fast algorithm matches the results of the astronomical algorithm."""
-    start = persiancalendar.ordinal_from_persian(
-        persiancalendar_fast.SUPPORTED_FIRST_YEAR, 1, 1)
-    end = persiancalendar.ordinal_from_persian(
-        persiancalendar_fast.SUPPORTED_LAST_YEAR + 1, 1, 1)
+    start = ordinal_from_persian(SUPPORTED_FIRST_YEAR, 1, 1)
+    end = ordinal_from_persian(SUPPORTED_LAST_YEAR + 1, 1, 1)
 
-    for date in range(start, end):
-        fast_p_date = persiancalendar_fast.persian_fast_from_ordinal(date)
-        astro_p_date = persiancalendar.persian_from_ordinal(date)
-        assert (fast_p_date == astro_p_date)
+    for ordinal in range(start, end):
+        assert persian_fast_from_ordinal(ordinal) == persian_from_ordinal(ordinal)
 
 
 if __name__ == "__main__":
